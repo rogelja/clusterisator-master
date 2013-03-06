aed95cbc4e3f05038b6c20e4f494560a225d1c14
rogelja <vrogelja@gmail.com>
2013-03-06 11:46:11 +0100

    Big up

--- projet_zz/MultiLevelKMInstance.cpp
+++ projet_zz/MultiLevelKMInstance.cpp
@@ -37,6 +37,124 @@ void MultiLevelAlgo::buildInstance(size_t level, KMInstance & instance,
 									   instance = KMInstance(_instance, aggregations);
 }
 
+// @brief construit une suite de problÃƒÂ¨mes agrÃƒÂ©gÃƒÂ©s en agrÃƒÂ©geant nbNodesMax noeuds par niveau et en produisant des graphes avec au plus nbNodes noeuds
+// @param nbNodes    : limite pour le graph le plus agrÃƒÂ©gÃƒÂ© 
+// @param nbNodesMax : limite max de noeuds agrÃƒÂ©gÃƒÂ© par ÃƒÂ©tape
+void MultiLevelAlgo::buildMultiLevelData(double nbNodes, double nbNodesMax) {
+#if 1
+	KMPartition partition(_instance, _instance.nbObs());
+	// on crÃƒÂ©e les singletons
+	for (size_t i(0); i < _instance.nbObs(); ++i)
+		partition.shift(i, i);
+
+	while (partition.nbLabels() > nbNodes) {
+		//		std::cout << "partition.nbLabels() : " << partition.nbLabels()
+		//				<< std::endl;
+		IndexedList used(partition.usedLabels());
+		// definit un nouveau niveau
+		_multiLevelConstraints.push_back(new KMConstraints(_input.nbObs()));
+		//
+		size_t compteur(0);
+		while (!used.empty() && compteur < nbNodesMax
+			&& partition.nbLabels() > nbNodes) {
+				size_t const m = used.pop_random();
+				if (!used.empty()) {
+					// calculer la distance de ce centre avec les autres
+					std::multimap<Double, size_t> neighbor;
+					for (auto const & c : partition.usedLabels()) {
+						if (m != c)
+							neighbor.insert(
+							std::make_pair(
+							partition.getDistanceCenter(m, c), c));
+					}
+					size_t const c(neighbor.begin()->second);
+					_multiLevelConstraints.back()->newCtr(
+						*partition.observations(m).begin(),
+						*partition.observations(c).begin());
+					partition.fusion(m, c);
+					// si plusieurs plusieurs plus pret : tirer au hazard (aprÃƒÂ©s)
+					used.erase(c);
+					compteur++;
+				}
+		};
+		// ajouter les contraintes associÃ©e Ã  ce niveau
+	};
+	std::cout << "nbNodes     " << partition.nbLabels() << std::endl;
+	std::cout << "nbNodesMax  " << nbNodesMax << std::endl;
+	std::cout << "Built       " << nbLevels() << " aggregation levels"
+		<< std::endl;
+#else
+	_multiLevelConstraints.push_back(new KMConstraints(_input.nbObs()));
+	_multiLevelConstraints.back()->newCtr(0,2);
+	_multiLevelConstraints.back()->newCtr(1,2);
+#endif
+}
+
+
+
+
+void MultiLevelAlgo::buildMultiLevelData_seuil(double nbNodes, double nbNodesMax) {
+#if 1
+	KMPartition partition(_instance, _instance.nbObs());
+
+	bool done =false;
+
+	// on crÃƒÂ©e les singletons
+	for (size_t i(0); i < _instance.nbObs(); ++i)
+		partition.shift(i, i);
+
+	while (partition.nbLabels() > nbNodes && !done) {
+		//		std::cout << "partition.nbLabels() : " << partition.nbLabels()
+		//				<< std::endl;
+		IndexedList used(partition.usedLabels());
+		done=true;
+		// definit un nouveau niveau
+		_multiLevelConstraints.push_back(new KMConstraints(_input.nbObs()));
+		//
+		size_t compteur(0);
+		_nbRejet=0;
+		while (!used.empty() && compteur < nbNodesMax
+			&& partition.nbLabels() > nbNodes) {
+				size_t const m = used.pop_random();
+				if (!used.empty()) {
+					// calculer la distance de ce centre avec les autres
+					std::multimap<Double, size_t> neighbor;
+					for (auto const & c : partition.usedLabels()) {
+						if (m != c)
+							neighbor.insert(
+							std::make_pair(
+							partition.getDistanceCenter(m, c), c));
+					}
+					size_t const c(neighbor.begin()->second);
+
+
+					if(partition.getDistanceCenter(m, c) < _seuil){
+
+						_multiLevelConstraints.back()->newCtr(
+							*partition.observations(m).begin(),
+							*partition.observations(c).begin());
+						partition.fusion(m, c);
+						// si plusieurs plusieurs plus pret : tirer au hazard (aprÃƒÂ©s)
+						used.erase(c);
+						compteur++;
+						done=false;
+					}
+					else
+						_nbRejet++;
+				}
+		};
+		// ajouter les contraintes associÃ©e Ã  ce niveau
+	};
+	std::cout << "nbNodes     " << partition.nbLabels() << std::endl;
+	std::cout << "nbNodesMax  " << nbNodesMax << std::endl;
+	std::cout << "Built       " << nbLevels() << " aggregation levels"
+		<< std::endl;
+#else
+	_multiLevelConstraints.push_back(new KMConstraints(_input.nbObs()));
+	_multiLevelConstraints.back()->newCtr(0,2);
+	_multiLevelConstraints.back()->newCtr(1,2);
+#endif
+}
 
 
 
@@ -45,6 +163,7 @@ void MultiLevelAlgo::buildMultiLevelData_tab(double nbNodes) {
 #if 1
 	// multimap pour avoir des doublons, on en a une par noeud qui contient le classement
 	std::vector< std::multimap<Double,size_t > > TabDistance(_instance.nbObs());	
+	std::vector<Double> seuils(_instance.nbObs(), 0);
 	Double const p(10);
 	// On calcule toutes les distances entres les points 
 	for (size_t i(0); i < _instance.nbObs(); ++i){
@@ -54,8 +173,9 @@ void MultiLevelAlgo::buildMultiLevelData_tab(double nbNodes) {
 			// insertion dans la map
 			TabDistance[i].insert(std::make_pair(d, j));
 			TabDistance[j].insert(std::make_pair(d, i));
-			
-
+			//il faut que chaque point ai un voisin au minimum
+			//donc si le seuil est plus petit que la distance mini
+			//pour un point et son voisin le plus proche, on réhausse le seuil
 			size_t n(0);
 			std::multimap<Double,size_t >::iterator it(TabDistance[i].begin());
 			// on ne garde que les 10 plus proches voisins 
@@ -66,25 +186,21 @@ void MultiLevelAlgo::buildMultiLevelData_tab(double nbNodes) {
 			TabDistance[i].erase(it, TabDistance[i].end());
 		}
 	}
-
-
-
-
+	// Ici on va essayer de parcourir, tant que necessaire, le tableau en aggrégeant pour chaque
+	// point le voisin qui lui ai le plus proche en les giclant de la map pour ne plus les reprendre ensuite
+	// il faudra biensur penser que la map est symétrique et donc i voisin de j alors j voisin de i .
+	// go .
 	KMPartition partition(_instance, _instance.nbObs());
 
-	// on crée les singletons
+	// on crÃƒÂ©e les singletons
 	for (size_t i(0); i < _instance.nbObs(); ++i)
 		partition.shift(i, i);
-	
-	
-	//on choisit de construire au maximum 3 niveaux ou un nb de points restants supérieur supérieur a nbNodes
-	
-
-	while (partition.nbLabels() > nbNodes ) {
+	while (partition.nbLabels() > nbNodes) {
+	std::cout << "Built       " << nbLevels() << " aggregation levels"	<< std::endl;
+	std::cout << "Remaining   " << partition.nbLabels()<< " nodes"	<< std::endl;
 		bool create(false);
 		IndexedList points(_instance.nbObs(), true);
 		while(! points.empty() && partition.nbLabels() > nbNodes  ){
-
 			size_t const point(points.pop_random());
 			for(auto const & voisin : TabDistance[point]){
 				if(points.contains(voisin.second)){
@@ -122,48 +238,75 @@ void MultiLevelAlgo::buildMultiLevelData_tab(double nbNodes) {
 // _startLevel : niveau de dÃƒÂ©part pour le raffinement
 // _startPoint : (attention doit ÃƒÂªtre compatible avec le niveau de plus agrÃƒÂ©gÃƒÂ©)
 void MultiLevelAlgo::refine() {
-
-
+	// lancer le KMEANS sur chaque niveau en partant du plus ÃƒÂ©levÃƒÂ© (celui qui contient le moins de noeuds)
+	// Ã  chaque fois on initialise avec le niveau prÃ©cÃ©dent (sauf le premier!)
+	// Pour le premier faire un appel Ã  random(0);
 	KMInstance instance;
 	Aggregations aggregations;
 	Timer timer;
-
-	//Pour chaque level construit 
-
+	//std::cout << "_input value : "<<_input.computeCost() << std::endl;
+	// pour chaque level
 	for (int level(_startLevel); level >= 0; level -= _step) {
-		
-		//on reconstruit le graphe avec les points agrégés
+		//for ( size_t level(_startLevel); level <= _multiLevelConstraints.size(); level+= _step) {
+		// ! on parcours Ã  l'envers
 		buildInstance(level, instance, aggregations);
-
-
+		//for (size_t i(0); i < aggregations.v.size(); ++i) {		
+		//	assert(!aggregations.v[i].empty());
+		//	size_t const l(_input.label(*aggregations.v[i].begin()));
+		//	
+		//	for(auto const & j : aggregations.v[i]){
+		//		assert(aggregations.newIds[j] == i);
+		//		assert( _input.label(j) == l);
+		//	}
+		//}
 		KMInput input(instance, _input.maxNbLabels());
-
-
-		// On applique le dernier partitionnement trouvé
+		//std::cout << input.centers() << std::endl;
+		//input.computeCenters();
+		//for (size_t i(0); i < _input.nbObs(); ++i) {
+		// il faut une solution faisable 
 		for (size_t i(0); i < aggregations.v.size(); ++i) {
 			input.shiftForced(i, _input.label(*aggregations.v[i].begin()));
 		}
-
-
-		//on initialise le compteur
+		//std::cout << input.centers() << std::endl;
+		//input.computeCenters();
+		//std::cout << input.centers() << std::endl;
+		//std::cout << " input value : "<< input.computeCost() << std::endl;
+		// on lance l'algo
 		timer.restart();
+		HMeans<false>()(input);
+		_stats[level] = MultiLevelAlgoStat(level, input.ite(), timer.elapsed(), input.cost());
 
-		// On applique les K-means pour rafiner le partitionnement
-		HMeans<true>()(input);
-
-		// On sauvegarde les mesures dans un tableau recapitulatif
-		_stats[level] = MultiLevelAlgoStat(level, input.ite(), timer.elapsed(),
-			input.cost());
-
-		//On replace les points dans la nouvelle classe qu'on lui a trouvé
+		// sauvegarde de la solution
+		//for (size_t i(0); i < _input.nbObs(); ++i) {
+		//	_input.shiftForced(i, input.label(aggregations.newIds[i]));
+		//}
 		for (size_t i(0); i < aggregations.v.size(); ++i) {
 			for (auto const & j : aggregations.v[i])
 				_input.shiftForced(j, input.label(i));
 		}
-
+		//for(auto const & i : aggregations.v){
+		//	assert(!i.empty());
+		//	size_t const l(_input.label(*i.begin()));
+		//	for(auto const & j : i)
+		//		assert( _input.label(j) == l);
+		//}
+		//std::cout << "_input value : "<<_input.computeCost() << std::endl;
+		// on stocke les stats
 	}
 
+	// pb ici traitement du dernier niveau
 
+	/*if(level>_multiLevelConstraints.size())
+	{
+	// on lance l'algo
+	HMeans<true>()(_input);
+	out()<<"Niveau de rafinement         : " <<0<<std::endl;
+	out()<<"Nombre d'iteration par etape : " <<_input.ite()<<std::endl;
+	out()<<"Temps ecoule                 : " <<timer.elapsed()<<std::endl;
+	out()<<"Valeur du cout               : " <<_input.cost()<<std::endl;
+	out()<<std::endl;
+
+	}*/
 }
 // @brief lance l'algorithme multi-niveau Ã  partir d'une suite d'agrÃ©gation, d'une partition de dÃ©part et d'un niveau de dÃ©part et avec un pas donnÃ©
 // _step: 
@@ -175,42 +318,71 @@ void MultiLevelAlgo::setOut(std::ostream & stream) {
 }
 void MultiLevelAlgo::launch() {
 	_stats.clear();
-	
 	std::cout << "Step  : " << _step << std::endl;
 	std::cout << "Level : " << _startLevel << std::endl;
-	
-	
 	// initialisation au point de depart
 	for (size_t i(0); i < _input.nbObs(); ++i) {
 		_input.shiftForced(i, _startPoint.label(i));
+		//file << i << " - "<< _startPoint.label(i)<<std::endl;
 	}
-
-
-	std::cout << "Starting point value : " << _input.computeCost() << std::endl;
+	//KMInstance instance;
+	//Aggregations aggregations;	
+	//for ( size_t level(0); level <= nbLevels(); level+= _step) {
+	//	std::cout << "checking " << level << std::endl;
+	//	buildInstance(level, instance,aggregations);
+	//	KMInput input(instance, _input.maxNbLabels());
+	//	//std::cout << input.centers() << std::endl;
+	//	//std::ofstream file(std::to_string(level).c_str());
+	//	//for(auto const & i : aggregations.v){
+	//	//	if(i.size()>1)
+	//	//	DisplayContainer(file , i);
+	//	//}
+	//	//file.close();
+	//	for(auto const & i : aggregations.v){
+	//		assert(!i.empty());
+	//		size_t const l(_input.label(*i.begin()));
+	//		//DisplayContainer(std::cout , i);
+	//		for(auto const & j : i)
+	//			assert( _input.label(j) == l);
+	//	}
+	//}
+	//std::cout << "Starting point value : " << _input.computeCost() << std::endl;
 
 	// lancement du run 
 	refine();
 
 }
-
-
 size_t MultiLevelAlgo::nbLevels() const {
 	return _multiLevelConstraints.size();
 }
-
-
-
 void MultiLevelAlgo::getStartPoint(Partition & point) {
 	KMInstance instance;
 	Aggregations aggregations;
 	buildInstance(nbLevels(), instance, aggregations);
 	KMInput input(instance, _input.maxNbLabels());
 	input.random(0);
+	//std::cout << input.centers()<<std::endl;
 	std::cout << "Starting point value : " << input.computeCost() << std::endl;
 	for (size_t i(0); i < _input.nbObs(); ++i) {
 		point.shift(i, input.label(aggregations.newIds[i]));
 	}
+	
 
+	//for (size_t level(0); level<=nbLevels(); ++level) {
+	//	buildInstance(level, instance,aggregations);
+	//	KMInput input2(instance, _input.maxNbLabels());			
+	//	for(size_t i(0); i<aggregations.v.size(); ++i){
+	//		input2.shiftForced(i, point.label(*aggregations.v[i].begin()));
+	//	}
+	//	//std::cout << input2.centers()<<std::endl;
+	//	std::cout << "Starting point value : "<<input2.computeCost() << std::endl;
+	//	for(auto const & i : aggregations.v){
+	//		assert(!i.empty());
+	//		size_t const l(_input.label(*i.begin()));
+	//		for(auto const & j : i)
+	//			assert( _input.label(j) == l);
+	//	}
+	//}
 }
 
 void MultiLevelAlgo::setStartLevel(size_t level) {
@@ -228,8 +400,23 @@ void MultiLevelAlgo::setStartPoint(Partition & point) {
 std::ostream & MultiLevelAlgo::out() {
 	return *_out;
 }
+
 MultiLevelAlgoStats const & MultiLevelAlgo::stats() const {
 	return _stats;
 }
 
 
+
+double MultiLevelAlgo::getSeuil(){
+	return _seuil;
+}
+
+void MultiLevelAlgo::setSeuil(double seuil){
+	_seuil=seuil;
+}
+
+
+
+size_t MultiLevelAlgo::getNbRejet(){
+	return _nbRejet;
+}
--- projet_zz/main.cpp
+++ projet_zz/main.cpp
@@ -22,6 +22,8 @@
 #include <fstream>
 
 
+double CalculSeuil(double ,RegisteredInstance );
+
 
 void usage() {
 	std::cout << "Available instances : \n";
@@ -46,242 +48,90 @@ template<class T> void WriteCsv(std::ostream & stream, T const & t, size_t n) {
 }
 
 int main(int argc, char ** argv) {
-
+	//usage();
 	//liste des instances a tester
-	std::list<size_t> list_instance;
-	//list_instance.push_back(0);
-	//list_instance.push_back(1);
-	//list_instance.push_back(4);
-	//list_instance.push_back(7);
-	list_instance.push_back(8);
-	//list_instance.push_back(9);
-	//list_instance.push_back(12);
-	//list_instance.push_back(15);
-	//list_instance.push_back(16);
-	//list_instance.push_back(19);
-	//list_instance.push_back(22);
-	//list_instance.push_back(25);
-	//list_instance.push_back(28);
-	//list_instance.push_back(29);
+	std::list<AvailableInstances> list_instance;
+	list_instance.push_back(AvailableInstances::segmentation);
 
+	// préparation de l'output qu iszera effacer à chaque run !!
 
-
-	//fichiers relevant les données de sortie et les statistiques.
 	std::ofstream file("output.csv");
 	std::ofstream stats("Stats.csv");
 
-
-	// les informations qui nous intéressent
-	file << "id;nom;n;k;Run;nbLevel;graine;";
+	file << "id;nom;n;k;Seed;nbLevel;";
 	file << "ite0;score0;CPU0;";
 	file << "start;";
 	file << "ite;score;CPU;";
+	//file << "Rapport Scores (%);Rapport iterations total(%); rapport iterations moyen(%);Rapport temps(%)";
 	file << std::endl;
-
-	//fichier de sortie pour les erreurs
+	// ce programme lance les tests sur toutes les instances.
 	std::ofstream debug("debug.log");
 
+	size_t const nbLancer ( 100);
+	
+	
+	size_t const kmin (3 );
+	size_t const kstep( 1 );
+	size_t const kmax ( 10);
 
-
-	//Nb de Run lancer pour les stats
-	size_t const nbLancer = 1;
-
-
-	// définit le nombre de classe de départ, celle de fin et le pas d'incrément
-	size_t const DepartClasse =10 ;
-	size_t const kmax = 100;
-	size_t const PasClasse =10 ;
-
-
-
-
-	for (auto const & i : list_instance) {
-		AvailableInstances id(static_cast<AvailableInstances>(i));
+	for (auto const & id : list_instance) {
 		RegisteredInstance instance(id);
 		instance.out();
+		// mais on arrete si notre algo n'est plus en multi-level.
+		size_t seed(1);
+		Number::SetSeed(seed);
+		for (size_t k(kmin); k <= kmax; k+= kstep) {		
+			std::map<size_t, MultiLevelAlgoStats> allStats;
+			std::map<size_t, size_t> sumIte;
+			std::map<size_t, double> sumsTime;
+			std::map<size_t, double> score;
 
-
-
-		std::map<size_t, double> sumsIte_moyenne[kmax/PasClasse];
-		std::map<size_t, size_t> nbIte_moyenne[kmax/PasClasse];
-		std::map<size_t, double> score_moyenne[kmax/PasClasse];
-		std::map<size_t, double> sumsTime_moyenne[kmax/PasClasse];
-		std::map<size_t, double> sumsIte_variance[kmax/PasClasse];
-		std::map<size_t, size_t> nbIte_variance[kmax/PasClasse];
-		std::map<size_t, double> score_variance[kmax/PasClasse];
-		std::map<size_t, double> sumsTime_variance[kmax/PasClasse];
-
-		std::map<size_t, MultiLevelAlgoStats> allStats[kmax/PasClasse][nbLancer];
-		std::map<size_t, double> sumsIte[kmax/PasClasse][nbLancer];
-		std::map<size_t, size_t> nbIte[kmax/PasClasse][nbLancer];
-		std::map<size_t, double> score[kmax/PasClasse][nbLancer];
-		std::map<size_t, double> sumsTime[kmax/PasClasse][nbLancer];
-		size_t NBLevel[kmax/PasClasse];
-		size_t Graine[nbLancer];
-
-
-		//size_t const kmax((size_t) std::ceil(instance.nbObs() * 0.15));
-
-		// on lance nbLancer avec un germe différent
-		for( size_t Lancer=0; Lancer < nbLancer ;Lancer++){
-
-
-			//initialisation du germe pour la partie aléatoire.
-			Graine[Lancer]=rand()+1;
-			Number::SetSeed(Graine[Lancer]);
-
-
-
-			for (size_t k(DepartClasse); k <= kmax; k+= PasClasse) {				
-
-
-				MultiLevelAlgo algo(instance, k);			
-
-
-
-				algo.setOut(debug);
-
-
-				//on agrège jusqu'a 20k noeuds ou alors jusqu'a deux niveaux
-				algo.buildMultiLevelData_tab(20*k);
-
-				Partition start(instance.nbObs(), k);
-
-
-				//generation du point de depart
-				algo.getStartPoint(start);		
-
-
-				NBLevel[k/PasClasse-1] = algo.nbLevels();
-				//on ne s'occupe aps encore du pas
-				//size_t const stepMax((size_t) std::ceil(algo.nbLevels() * 0.10));
-				size_t step=1;
-				algo.setStep(step);
-
-				for (size_t level(0); level <= algo.nbLevels(); ++level) {
-					algo.setStartPoint(start);
-					algo.setStartLevel(level);
-					algo.launch();
-					allStats[k/PasClasse-1][Lancer][level] = algo.stats();
+			std::cout << " ------------------------"<<k<<std::endl;
+			MultiLevelAlgo algo(instance, k);
+			//algo.setSeuil(seuil);				
+			algo.setOut(debug);
+			algo.buildMultiLevelData_tab(20 * k);							
+			//algo.buildMultiLevelData_tab(20);
+			// on agrege 20k des noeuds par palier de 5% des noeuds totaux 
+			//algo.buildMultiLevelData(20 * k, amax);
+			Partition start(instance.nbObs(), k);
+			//generation du point de depart
+			algo.getStartPoint(start);		
+			//on ne s'occupe aps encore du pas
+			//size_t const stepMax((size_t) std::ceil(algo.nbLevels() * 0.10));
+			for (size_t level(0); level <= algo.nbLevels(); ++level) {
+				algo.setStartPoint(start);
+				algo.setStartLevel(level);
+				algo.launch();
+				allStats[level] = algo.stats();
+				// calcul des stats				
+				sumIte[level] = 0;
+				sumsTime[level] = 0;
+				for(auto const & it : algo.stats()){
+					sumIte[level] += it.second._ite;
+					sumsTime[level] += it.second._time;					
 				}
+				score[level] = algo.stats().begin()->second._cost;
+			}
+			for (size_t level(0); level <= algo.nbLevels(); ++level) {				
+				WriteCsv(file, id, 6);
+				WriteCsv(file, instance.name);
+				WriteCsv(file, instance.nbObs());
+				WriteCsv(file, k);
+				WriteCsv(file, seed);
+				WriteCsv(file, algo.nbLevels());
 
-
-				//on calcule les différentes valeurs intéréssantes pour nos analyses
-
-				for (auto const & stat : allStats[k/PasClasse-1][Lancer]) {
-					size_t const level(stat.first);
-					nbIte[k/PasClasse-1][Lancer][level] = allStats[k/PasClasse-1][Lancer][level].size();
-					score[k/PasClasse-1][Lancer][level] = allStats[k/PasClasse-1][Lancer][level].begin()->second._cost;
-					for (auto & stat : allStats[k/PasClasse-1][Lancer][level]) {
-						sumsIte[k/PasClasse-1][Lancer][level] += stat.second._ite;
-						sumsTime[k/PasClasse-1][Lancer][level] += stat.second._time;
-					}
-				}
-
+				WriteCsv(file, sumIte[0]);
+				WriteCsv(file, score[0], 15);
+				WriteCsv(file, sumsTime[0], 6);
+				WriteCsv(file, level);
+				WriteCsv(file, sumIte[level]);
+				WriteCsv(file, score[level], 15);
+				WriteCsv(file, sumsTime[level], 6);
+				file << std::endl;
 			}
 		}
-
-
-		//Cette partie calcule les moyennes et variances des données relevées pendant les tests et les
-		//rédige dans les fichiers output.csv et stats.csv .
-
-		for (size_t k(DepartClasse); k <= kmax; k+= PasClasse) {
-			size_t const levelMax(NBLevel[k/PasClasse-1]);
-			for( size_t level=0; level <= levelMax ;level++){
-				for( size_t Lancer=0; Lancer < nbLancer ;Lancer++){
-					WriteCsv(file, i, 6);
-					WriteCsv(file, instance.name);
-					WriteCsv(file, instance.nbObs());
-					WriteCsv(file, k);
-					WriteCsv(file, Lancer);
-					WriteCsv(file, levelMax);
-					WriteCsv(file, Graine[Lancer]);
-
-					WriteCsv(file, sumsIte[k/PasClasse-1][Lancer][0]);
-					WriteCsv(file, score[k/PasClasse-1][Lancer][0], 15);
-					WriteCsv(file, sumsTime[k/PasClasse-1][Lancer][0], 6);
-					WriteCsv(file, level);
-					WriteCsv(file, sumsIte[k/PasClasse-1][Lancer][level]);
-					WriteCsv(file, score[k/PasClasse-1][Lancer][level], 15);
-					WriteCsv(file, sumsTime[k/PasClasse-1][Lancer][level], 6);
-					file << std::endl;
-
-
-					sumsIte_moyenne[k/PasClasse-1][level] += sumsIte[k/PasClasse-1][Lancer][level]/((double)nbLancer);
-					nbIte_moyenne[k/PasClasse-1][level] += nbIte[k/PasClasse-1][Lancer][level]/nbLancer;
-					score_moyenne[k/PasClasse-1][level] += score[k/PasClasse-1][Lancer][level]/((double)nbLancer);
-					sumsTime_moyenne[k/PasClasse-1][level] += sumsTime[k/PasClasse-1][Lancer][level]/((double)nbLancer);
-				}
-			}
-		}
-		file << std::endl;
-
-		// calcul de la variance par la formule E[(X- E[X])²]
-
-
-		for (size_t k(DepartClasse); k <= kmax; k+= PasClasse) {
-			size_t const levelMax(NBLevel[k/PasClasse-1]);
-			for( size_t level=0; level <= levelMax ;level++){
-
-				for( size_t Lancer=0; Lancer < nbLancer ;Lancer++){
-
-					sumsIte_variance[k/PasClasse-1][level] += (sumsIte[k/PasClasse-1][Lancer][level]-sumsIte_moyenne[k/PasClasse-1][level])*
-						(sumsIte[k/PasClasse-1][Lancer][level]-sumsIte_moyenne[k/PasClasse-1][level])/
-						((double)nbLancer);
-					nbIte_variance[k/PasClasse-1][level] += (nbIte[k/PasClasse-1][Lancer][level]-nbIte_moyenne[k/PasClasse-1][level])*
-						(nbIte[k/PasClasse-1][Lancer][level]-nbIte_moyenne[k/PasClasse-1][level])/
-						nbLancer;
-					score_variance[k/PasClasse-1][level] += (score[k/PasClasse-1][Lancer][level]-score_moyenne[k/PasClasse-1][level])*
-						(score[k/PasClasse-1][Lancer][level]-score_moyenne[k/PasClasse-1][level])/
-						((double)nbLancer);
-					sumsTime_variance[k/PasClasse-1][level] += (sumsTime[k/PasClasse-1][Lancer][level]-sumsTime_moyenne[k/PasClasse-1][level])*
-						(sumsTime[k/PasClasse-1][Lancer][level]-sumsTime_moyenne[k/PasClasse-1][level])/
-						((double)nbLancer);
-
-
-				}
-			}
-		}
-
-		stats << "id;nom;n;k;NbRun;nbLevel;";
-		stats << "ite0_moyenne;ite0_variance;score0_moyenne;score0_variance;CPU0_moyenne;CPU0_variance;";
-		stats << "start;";
-		stats << "ite_moyenne;ite_variance;score_moyenne;score_variance;CPU_moyenne;CPU_variance;";
-		stats << std::endl;
-
-		for (size_t k(DepartClasse); k <= kmax; k+= PasClasse) {
-			size_t const levelMax(NBLevel[k/PasClasse-1]);
-			for( size_t level=0; level <= levelMax ;level++){
-
-				WriteCsv(stats, i, 6);
-				WriteCsv(stats, instance.name);
-				WriteCsv(stats, instance.nbObs());
-				WriteCsv(stats, k);
-				WriteCsv(stats, nbLancer);
-				WriteCsv(stats, levelMax);
-
-				WriteCsv(stats, sumsIte_moyenne[k/PasClasse-1][0]);
-				WriteCsv(stats, sumsIte_variance[k/PasClasse-1][0]);
-				WriteCsv(stats, score_moyenne[k/PasClasse-1][0], 15);
-				WriteCsv(stats, score_variance[k/PasClasse-1][0], 15);
-				WriteCsv(stats, sumsTime_moyenne[k/PasClasse-1][0], 6);
-				WriteCsv(stats, sumsTime_variance[k/PasClasse-1][0], 6);
-				WriteCsv(stats, level);
-				WriteCsv(stats, sumsIte_moyenne[k/PasClasse-1][level]);
-				WriteCsv(stats, sumsIte_variance[k/PasClasse-1][level]);
-				WriteCsv(stats, score_moyenne[k/PasClasse-1][level], 15);
-				WriteCsv(stats, score_variance[k/PasClasse-1][level], 15);
-				WriteCsv(stats, sumsTime_moyenne[k/PasClasse-1][level], 6);
-				WriteCsv(stats, sumsTime_variance[k/PasClasse-1][level], 6);
-
-				stats << std::endl;
-			}
-		}
-
-
 	}
-
-
 	file.close();
 	stats.close();
 	debug.close();
@@ -289,6 +139,3 @@ int main(int argc, char ** argv) {
 	//	std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 	return 0;
 }
-
-
-
